09a1c66d9a7d2fd8d40fec49db80ff13e8234005
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-14 20:03:23 +0500

    `Tab Layout and Screens Added`

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Tabs } from "expo-router";
 import HomeScreen from ".";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
 
 type Props = {};
 
@@ -25,7 +26,44 @@ const TabLayout = (props: Props) => {
         }}
       />
 
-      
+      <Tabs.Screen
+        name="communities"
+        options={{
+          title: "Communities",
+          tabBarIcon: ({ color }) => (
+            <Feather name="users" size={24} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="plus" size={24} color={color} />
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          tabBarIcon: ({ color }) => (
+            <Feather name="users" size={24} color={color} />
+          )
+        }}
+      />
+
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color }) => (
+            <Feather name="bell" size={24} color={color} />
+          )
+        }}
+      />
     </Tabs>
   );
 };
