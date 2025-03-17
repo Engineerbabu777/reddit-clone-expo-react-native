19f6e2836c427ddc618106dcaf8c2cb0184ab3bf
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-17 21:08:40 +0500

    `Fetching Data From Supabase`

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,9 +1,24 @@
 import { FlatList, View } from "react-native";
 import PostListItem from "../../../components/PostItem";
-import posts from "../../../../assets/data/posts.json";
 import { Post } from "../../../../types";
 
+import { supabase } from "../../../lib/supabse";
+import { useEffect, useState } from "react";
+
 export default function HomeScreen() {
+  const [posts, setPosts] = useState<any>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*, group:groups(*), user:users!posts_user_id_fkey(*)");
+    setPosts(data);
+  };
+
   return (
     <View>
       <FlatList
